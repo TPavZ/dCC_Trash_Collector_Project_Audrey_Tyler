da0d0a3b43bdc62adc64fceaa99b29b990feab1a
Audrey Schmidt <audsch@outlook.com>
2021-12-14 13:59:50 -0600

    created employee index view that shows customer pickups to be completed for the day

--- trash_collector/employees/views.py
+++ trash_collector/employees/views.py
@@ -5,25 +5,38 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth.decorators import login_required
 from datetime import date
 from django.apps import apps
-
 from .models import Employee
+import calendar
+#from trash_collector.customers.models import Customer
 
 # Create your views here.
-
 # TODO: Create a function for each path created in employees/urls.py. Each will need a template as well.
-
-
-  
 
 @login_required
 def index(request):
     # The following line will get the logged-in user (if there is one) within any view function
     logged_in_user = request.user
     try:
-        # This line will return the customer record of the logged-in user if one exists
-        logged_in_customer = Employee.objects.get(user=logged_in_user)
+        # This line will return the employee record of the logged-in user if one exists
+        logged_in_employee = Employee.objects.get(user=logged_in_user)
+        today = date.today()
+        today_weekday_number = today.weekday()
+        today_weekday = calendar.day_name[today_weekday_number]
+
         Customer = apps.get_model('customers.Customer')
-        return render(request, 'employees/index.html')
+        
+        customer_weekday_pickup = Customer.objects.filter(zip_code=logged_in_employee.zip_code).filter(weekly_pickup=today_weekday).exclude(date_of_last_pickup=today).exclude(suspend_start__lte=today, suspend_end__gte=today)
+        customer_onetime_pickup = Customer.objects.filter(zip_code=logged_in_employee.zip_code).filter(one_time_pickup=today).exclude(date_of_last_pickup=today).exclude(suspend_start__lte=today, suspend_end__gte=today)
+        
+        data_visualization = [item for item in customer_weekday_pickup]
+        
+        context ={
+            'logged_in_employee': logged_in_employee,
+            'today': today,
+            'customer_weekday_pickup': customer_weekday_pickup,
+            'customer_onetime_pickup': customer_onetime_pickup,
+        }
+        return render(request, 'employees/index.html', context)
 
         
     except ObjectDoesNotExist:
