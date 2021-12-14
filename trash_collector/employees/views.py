c97a2f1575b1e6dcc52504babce693e605721f14
TPavZ <tmpavlic@gmail.com>
2021-12-14 11:22:45 -0600

    working add employee

--- trash_collector/employees/views.py
+++ trash_collector/employees/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth.decorators import login_required
+from datetime import date
 from django.apps import apps
 
 from .models import Employee
@@ -11,10 +13,21 @@ from .models import Employee
 # TODO: Create a function for each path created in employees/urls.py. Each will need a template as well.
 
 
+  
+
+@login_required
 def index(request):
-    # This line will get the Customer model from the other app, it can now be used to query the db for Customers
-    Customer = apps.get_model('customers.Customer')
-    return render(request, 'employees/index.html')
+    # The following line will get the logged-in user (if there is one) within any view function
+    logged_in_user = request.user
+    try:
+        # This line will return the customer record of the logged-in user if one exists
+        logged_in_customer = Employee.objects.get(user=logged_in_user)
+        Customer = apps.get_model('customers.Customer')
+        return render(request, 'employees/index.html')
+
+        
+    except ObjectDoesNotExist:
+        return HttpResponseRedirect(reverse('employees:create'))
 
 @login_required
 def create(request):
